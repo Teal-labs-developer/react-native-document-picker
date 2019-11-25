84a1bec4785e48453914b4b548dd51c2c08bf6f5
dhrumilpatel <dhrumil@toddleapp.com>
2019-11-25 17:58:43 +0530

    Result of activity converted to file-picker type output

--- android/src/main/java/io/github/elyx0/reactnativedocumentpicker/DocumentPickerModule.java
+++ android/src/main/java/io/github/elyx0/reactnativedocumentpicker/DocumentPickerModule.java
@@ -1,15 +1,20 @@
 package io.github.elyx0.reactnativedocumentpicker;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.util.Log;
 
@@ -24,6 +29,14 @@ import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * @see <a href="https://developer.android.com/guide/topics/providers/document-provider.html">android documentation</a>
@@ -137,6 +150,111 @@ public class DocumentPickerModule extends ReactContextBaseJavaModule {
 		}
 	}
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final String[] split = id.split(":");
+                final String type = split[0];
+                if ("raw".equalsIgnoreCase(type)) {
+                    return split[1];
+                } else {
+                    String prefix = Build.VERSION.SDK_INT >= Build.VERSION_CODES.O ? "file:///" : "content://";
+                    final Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse(prefix + "downloads/public_downloads"), Long.valueOf(id));
+
+                    return getDataColumn(context, contentUri, null, null);
+                }
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+	private WritableMap getDataFromURI(Uri uri) {
+		WritableMap map = Arguments.createMap();
+		Activity currentActivity = getCurrentActivity();
+		map.putString("uri", uri.toString());
+		String path = null;
+		String readableSize = null;
+		Long size = null;
+		path = getPath(currentActivity, uri);
+		if (path != null) {
+			map.putString("path", path);
+			readableSize = getFileReadableSize(path);
+			size = getFileSize(path);
+			map.putString("readableSize", readableSize);
+			map.putInt("size", size.intValue());
+		} else {
+			path = getFileFromUri(currentActivity, uri);
+			if (!path.equals("error")) {
+				readableSize = getFileReadableSize(path);
+				size = getFileSize(path);
+				map.putString("path", path);
+				map.putString("readableSize", readableSize);
+				map.putInt("size", size.intValue());
+			}
+		}
+		map.putString("type", currentActivity.getContentResolver().getType(uri));
+		map.putString("fileName", getFileNameFromUri(currentActivity, uri));
+		return map;
+	}
+
 	public void onShowActivityResult(int resultCode, Intent data, Promise promise) {
 		if (resultCode == Activity.RESULT_CANCELED) {
 			promise.reject(E_DOCUMENT_PICKER_CANCELED, "User canceled document picker");
@@ -153,12 +271,14 @@ public class DocumentPickerModule extends ReactContextBaseJavaModule {
 				WritableArray results = Arguments.createArray();
 
 				if (uri != null) {
-					results.pushMap(getMetadata(uri));
+                    WritableMap doc = getDataFromURI(uri);
+					results.pushMap(doc);
 				} else if (clipData != null && clipData.getItemCount() > 0) {
 					final int length = clipData.getItemCount();
 					for (int i = 0; i < length; ++i) {
-						ClipData.Item item = clipData.getItemAt(i);
-						results.pushMap(getMetadata(item.getUri()));
+                        ClipData.Item item = clipData.getItemAt(i);
+                        WritableMap doc = getDataFromURI(item.getUri());
+                        results.pushMap(doc);
 					}
 				} else {
 					promise.reject(E_INVALID_DATA_RETURNED, "Invalid data returned by intent");
@@ -212,4 +332,123 @@ public class DocumentPickerModule extends ReactContextBaseJavaModule {
 
 		return map;
 	}
+
+	private String getFileReadableSize(String path) {
+		File file = new File(path);
+		long size = FileUtils.sizeOf(file);
+		return FileUtils.byteCountToDisplaySize(size);
+	}
+
+	private Long getFileSize (String path) {
+		File file = new File(path);
+		return file.length();
+	}
+
+	private String getFileFromUri(Activity activity, Uri uri) {
+		//If it can't get path of file, file is saved in cache, and obtain path from there
+		try {
+			String filePath = activity.getCacheDir().toString();
+			String fileName = getFileNameFromUri(activity, uri);
+			String path = filePath + "/" + fileName;
+			if (!fileName.equals("error") && saveFileOnCache(path, activity, uri)) {
+				return path;
+			} else {
+				return "error";
+			}
+		} catch (Exception e) {
+			//Log.d("FilePickerModule", "Error getFileFromStream");
+			return "error";
+		}
+	}
+
+	private String getFileNameFromUri(Activity activity, Uri uri) {
+		Cursor cursor = activity.getContentResolver().query(uri, null, null, null, null);
+		if (cursor != null && cursor.moveToFirst()) {
+			final int column_index = cursor.getColumnIndexOrThrow("_display_name");
+			return cursor.getString(column_index);
+		} else {
+			return "error";
+		}
+	}
+
+	private boolean saveFileOnCache(String path, Activity activity, Uri uri) {
+		//Log.d("FilePickerModule", "saveFileOnCache path: "+path);
+		try {
+			InputStream is = activity.getContentResolver().openInputStream(uri);
+			OutputStream stream = new BufferedOutputStream(new FileOutputStream(path));
+			int bufferSize = 1024;
+			byte[] buffer = new byte[bufferSize];
+			int len = 0;
+			while ((len = is.read(buffer)) != -1) {
+				stream.write(buffer, 0, len);
+			}
+
+			if (stream != null)
+				stream.close();
+
+			//Log.d("FilePickerModule", "saveFileOnCache done!");
+			return true;
+
+		} catch (Exception e) {
+			//Log.d("FilePickerModule", "saveFileOnCache error");
+			return false;
+		}
+	}
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } catch (Exception e) {
+            Log.e("FilePickerModule", "Failed to get cursor, so return null for path", e);
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
 }
